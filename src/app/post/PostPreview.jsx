3948069233c7839af866dc2dae39ae13a9015a94
shen <solove5171@gmail.com>
2025-07-18 15:21:31 +0800

    feat(PostPage): add PostPreview component to display posts dynamically

---
+++ src/app/post/PostPreview.jsx
@@ -0,0 +1,20 @@
+const PostPreview = ({ post }) => {
+  return (
+    <>
+      <div className="post-preview">
+        <a href="post.html">
+          <h2 className="post-title">{post.title}</h2>
+          {/* 有些文章有subTitle */}
+          {post.subTitle && <h3 className="post-subtitle">{post.subTitle}</h3>}
+        </a>
+        <p className="post-meta">
+          Posted by
+          <a href="#!">{post.auther}</a>
+          on {post.date}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default PostPreview;
--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,4 +1,47 @@
 import Hero from "@/components/Hero";
+import PostPreview from "./PostPreview";
+const POSTS = [
+  {
+    title: "Man must explore, and this is exploration at its greatest",
+    subTitle: "Problems look mighty small from 150 miles up",
+    content:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    author: "Start Bootstrap",
+    date: "September 24, 2023",
+    id: 0,
+  },
+  {
+    title:
+      "I believe every human has a finite number of heartbeats. I don'd not intend to waste any of mine.",
+    subTitle: "",
+    author: "Start Bootstrap",
+    content:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    date: "September 18, 2023",
+    id: 1,
+  },
+  {
+    title: "Science has not yet mastered prophecy",
+    subTitle:
+      "We predict too much for the next year and yet far too little for the next ten.",
+    content:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    author: "Start Bootstrap",
+    date: "August 24, 2023",
+    id: 2,
+  },
+  {
+    title: "Failure is not an option",
+    subTitle:
+      "Many say exploration is part of our destiny, but it’s actually our duty to future generations.",
+    author: "Start Bootstrap",
+    content:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    date: "July 8, 2023",
+    id: 3,
+  },
+];
+
 const PostPage = () => {
   return (
     <>
@@ -6,68 +49,14 @@ const PostPage = () => {
       <div className="container px-4 px-lg-5">
         <div className="row gx-4 gx-lg-5 justify-content-center">
           <div className="col-md-10 col-lg-8 col-xl-7">
-            <div className="post-preview">
-              <a href="post.html">
-                <h2 className="post-title">
-                  Man must explore, and this is exploration at its greatest
-                </h2>
-                <h3 className="post-subtitle">
-                  Problems look mighty small from 150 miles up
-                </h3>
-              </a>
-              <p className="post-meta">
-                Posted by
-                <a href="#!">Start Bootstrap</a>
-                on September 24, 2023
-              </p>
-            </div>
-            <hr className="my-4" />
-            <div className="post-preview">
-              <a href="post.html">
-                <h2 className="post-title">
-                  I believe every human has a finite number of heartbeats. I
-                  intend to waste any of mine.
-                </h2>
-              </a>
-              <p className="post-meta">
-                Posted by
-                <a href="#!">Start Bootstrap</a>
-                on September 18, 2023
-              </p>
-            </div>
-            <hr className="my-4" />
-            <div className="post-preview">
-              <a href="post.html">
-                <h2 className="post-title">
-                  Science has not yet mastered prophecy
-                </h2>
-                <h3 className="post-subtitle">
-                  We predict too much for the next year and yet far too little
-                  for the next ten.
-                </h3>
-              </a>
-              <p className="post-meta">
-                Posted by
-                <a href="#!">Start Bootstrap</a>
-                on August 24, 2023
-              </p>
-            </div>
-            <hr className="my-4" />
-            <div className="post-preview">
-              <a href="post.html">
-                <h2 className="post-title">Failure is not an option</h2>
-                <h3 className="post-subtitle">
-                  Many say exploration is part of our destiny, but it’s actually
-                  our duty to future generations.
-                </h3>
-              </a>
-              <p className="post-meta">
-                Posted by
-                <a href="#!">Start Bootstrap</a>
-                on July 8, 2023
-              </p>
-            </div>
-            <hr className="my-4" />
+            {POSTS.map((post, index) => {
+              return (
+                <>
+                  <PostPreview post={post} key={post.id} />
+                  {index !== post.index - 1 && <hr className="my-4" />}
+                </>
+              );
+            })}
             <div className="d-flex justify-content-end mb-4">
               <a className="btn btn-primary text-uppercase" href="#!">
                 Older Posts →
