940a019d8e5a852db0c49caf5197ed6468388bdd
shen <solove5171@gmail.com>
2025-07-22 15:26:11 +0800

    fix(Navbar): update link to point to the correct posts route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
               </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link px-lg-3 py-3 py-lg-4" href="post.html">
+              <a className="nav-link px-lg-3 py-3 py-lg-4" href="/posts">
                 Sample Post
               </a>
             </li>
